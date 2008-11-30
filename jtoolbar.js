e12d141f54d0fde3fa55303dc95f4841e530cd42
David Underhill <dgu@cs.stanford.edu>
2008-11-30 10:48:43 -0800

    added comments to fields in constructor

--- jtoolbar.js
+++ jtoolbar.js
@@ -36,13 +36,28 @@ var JTB = function() {
          * @param ...       remaining arguments specify name-link pairs
          */
         Toolbar : function(div_name) {
+            /* name of the div which contains the toolbar */
             this.div_id         = div_name;
+
+            /* where to place our div relative to its parent */
             this.dock           = JTB.DOCK_BOTTOM;
+
+            /* whether to show the pin/unpin graphic */
             this.show_pin       = true;
+
+            /* whether the toolbar is currently pinned */
             this.pinned         = true;
+
+            /* whether the toolbar is visible, invis, or transitioning */
             this.state          = JTB.STATE_VIS;
+
+            /* number of milliseconds for the toolbar to slide in/out */
             this.animation_time = 250;
+
+            /* pixels from the edge which triggers the toolbar to show */
             this.trigger_dist   = 100;
+
+            /* array of ToolbarLink objects to show in the toolbar div */
             this.links          = [];
 
             /* create the div if it doesn't exist */
