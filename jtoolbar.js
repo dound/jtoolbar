a4842d49d9a4ef0c91589be5a86098e879e9c839
David Underhill <dgu@cs.stanford.edu>
2008-12-05 12:27:36 -0800

    use setFloatPos in setFloatPosX/Y

--- jtoolbar.js
+++ jtoolbar.js
@@ -584,7 +584,7 @@ var JTB = function() {
 
             /** set the float x offset of this toolbar from its parent */
             JTB.Toolbar.prototype.setFloatPosX = function(x) {
-                this.floatx = x;
+                this.setFloatPos(x, this.floaty);
                 return this;
             };
 
@@ -595,7 +595,7 @@ var JTB = function() {
 
             /** set the float y offset of this toolbar from its parent */
             JTB.Toolbar.prototype.setFloatPosY = function(y) {
-                this.floaty = y;
+                this.setFloatPos(this.floatx, y);
                 return this;
             };
 
@@ -603,6 +603,7 @@ var JTB = function() {
             JTB.Toolbar.prototype.setFloatPos = function(x, y) {
                 this.floatx = x;
                 this.floaty = y;
+                this.refreshGfx();
                 return this;
             };
 
